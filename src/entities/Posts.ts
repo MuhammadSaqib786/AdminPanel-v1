8d479815b020451d90cc6eee159e679207cd090a
Dlinh <duylinh196tb@gmail.com>
2023-03-29 12:04:06 +0700

    add typeorm

---
+++ src/entities/Posts.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  text: string;
+}
